c5b077bdee45ce2acfec4cef49a72634f689597a
silindaM <silindanemdu@gmail.com>
2021-08-31 22:09:18 +0200

    view product class

---
+++ src/app/Components/view/view.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ProductService } from 'src/app/Services/product.service';
+
+@Component({
+  selector: 'app-view',
+  templateUrl: './view.component.html',
+  styleUrls: ['./view.component.css']
+})
+export class ViewComponent implements OnInit {
+
+  public products:any=[];
+  constructor(private servi:ProductService,private router:Router) { }
+
+  ngOnInit(): void {
+    //when applications starts load product
+    this.loadProducts();
+  }
+  //load Products
+  loadProducts(){
+    this.servi.getProducts().subscribe(
+      data=>{
+        this.products=data;
+        console.log(data);
+      },
+      error=>{
+        console.log(error);
+      });
+  }
+  //go to select item
+  selectedProduct(link:any,id:any){
+    //if not item selected
+    if(id===''){
+      this.router.navigate([link]);
+    }
+    //if item is selected
+    else{
+      this.router.navigate([link +'/'+ id]);
+    }
+  }
+
+}
