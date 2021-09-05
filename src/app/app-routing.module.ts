f8446ab1c940ca066834924160a78a19a3a5c396
silindaM <silindanemdu@gmail.com>
2021-09-05 23:56:52 +0200

    fix routing modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateComponent } from './Components/create/create.component';
+import { DetailsComponent } from './Components/details/details.component';
 import { UpdateComponent } from './Components/update/update.component';
 import { ViewComponent } from './Components/view/view.component';
 
 const routes: Routes = [
   {path:'',component:ViewComponent},
   {path:'create',component:CreateComponent},
-  {path:'update/:id',component:UpdateComponent},
-  {path:'view/:id',component:ViewComponent}
+  {path:'details/:id',component:DetailsComponent},
+  {path:'update',component:UpdateComponent}
 ];
 
 @NgModule({
