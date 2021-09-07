31d2231e94df4525b216e98a40eb55116cc2103e
silindaM <silindanemdu@gmail.com>
2021-09-07 20:33:02 +0200

    add content to update

--- src/app/Components/update/update.component.ts
+++ src/app/Components/update/update.component.ts
@@ -1,61 +1,77 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
-import { ProductService } from 'src/app/Services/product.service';
+import {Router, ActivatedRoute} from "@angular/router";
+import {ProductService} from "../../Services/product.service";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import { Product } from 'src/app/Model/product';
 
 @Component({
-  selector: 'app-update',
-  templateUrl: './update.component.html',
-  styleUrls: ['./update.component.css']
+selector: 'app-update',
+templateUrl: './update.component.html',
+styleUrls: ['./update.component.css']
 })
 export class UpdateComponent implements OnInit {
-
-  productForm?:FormGroup;
-  productId:any;
-  productData:any;
-  constructor(private servi:ProductService,private router:Router,private actived:ActivatedRoute,private fb:FormBuilder) {
-    
-    this.productForm=this.fb.group({
-      name:['',Validators.required],
-      description:['',Validators.required],
-      price:['',Validators.required]
+productForm: FormGroup;
+productID:any;
+productData?: Product;
+constructor( private fb: FormBuilder,private crudService: ProductService,private router: Router,private actRoute: ActivatedRoute) {
+this.productForm = this.fb.group({
+      name: ['', Validators.required],
+      description: ['', Validators.required],
+      price: ['', Validators.required],
     });
-   }
-   //load product details
-   loadDetails(id:any){
-      this.servi.getProductDetails(id).subscribe(
-        data=>{
-          this.productData=data;
-          this.productForm?.controls['name'].setValue(this.productData['name']);
-          this.productForm?.controls['description'].setValue(this.productData['description']);
-          this.productForm?.controls['price'].setValue(this.productData['price']);
-       
-        }
-      )
-   }
-   //update data
-   updateData(values:any){
-     const productData=new FormData();
-     productData.append('id',this.productId);
-     productData.append('name',values.name);
-     productData.append('description',values.description);
-     productData.append('price',values.price);
+//code
+}
+get ProductName(){
+  return this.productForm.get('name');
+}
+get ProductDescription(){
+  return this.productForm.get('description');
+}
+get ProductPrice(){
+  return this.productForm.get('price');
+}
 
-     this.servi.updateProduct(productData).subscribe(
-       data=>{
-         console.log(data);
-         this.router.navigate(['']);
-       },
-       error=>{
-         console.log(error);
-       }
-     )
-   }
+      loadProductDetails(productID:any){
+            this.crudService.getProductDetails(productID).subscribe
+            (product => {
+              this.productData = new Product();
+              this.productForm.controls['name'].setValue(this.productData['name']);
+              this.productForm.controls['description'].setValue(this.productData['description']);
+              this.productForm.controls['price'].setValue(this.productData['price']);
+              this.productData=product;
+        });
+}
+updateProductData(values:any){
+  const productData = new FormData();
+ 
+  this.productID=this.actRoute.snapshot.params['id'];
+  productData.append('name',values.name);
+  productData.append('description',values.description);
+  productData.append('price', values.price);
 
-  ngOnInit(): void {
-    //get current id
-    this.productId=this.actived.snapshot.params['id'];
-    this.loadDetails(this.productId);
+  this.crudService.updateProduct(this.productID,productData).subscribe
+  (result=>{
+    this.productData=new Product();
+    this.router.navigate(['']);
+  });
   }
-
+ngOnInit() {
+  this.productID = this.actRoute.snapshot.params['id'];
+  this.crudService.getProductDetails(this.productID).subscribe(
+    data=>{
+      this.productData=data;
+    },
+    error=>{
+      console.log(error);
+    });
+  }
+  onSubmit(){
+    console.log(this.productForm.value);
+    this.crudService.updateProduct(this.productID,this.productForm.value).subscribe(
+      res=>{
+        console.log("welcome");
+        this.router.navigate(['']);
+      }
+    )
+  }
 }
